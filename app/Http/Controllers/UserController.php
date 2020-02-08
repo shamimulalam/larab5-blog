2561bc2146e1a0cbd570036fa15cb834bcfa152e
Trainer PNT <trainer@piit.local>
2020-02-08 13:50:56 +0600

    User edit done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -66,9 +66,10 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(User $user)
     {
-        //
+        $data['user'] = $user;
+        return view('admin.user.edit',$data);
     }
 
     /**
@@ -78,9 +79,15 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,'.$user->id,
+            'phone' => 'required|unique:users,phone,'.$user->id
+        ]);
+        $user->update($request->all());
+        return redirect()->route('user.index');
     }
 
     /**
--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -717,12 +717,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0 text-dark">User List</h1>
+                        <h1 class="m-0 text-dark">Edit User</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">User List</li>
+                            <li class="breadcrumb-item active">Edit User</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
@@ -736,52 +736,49 @@
 
                 <div class="row">
                     <!-- left column -->
-                    <div class="col-md-12">
-                        <div class="card">
+                    <div class="offset-3 col-md-6">
+                        <!-- general form elements -->
+                        <div class="card card-primary">
                             <div class="card-header">
-                                <h3 class="card-title">All Users</h3>
-
-                                <div class="card-tools">
-                                    <ul class="pagination pagination-sm float-right">
-                                        <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
-                                    </ul>
-                                </div>
+                                <h3 class="card-title">Edit User</h3>
                             </div>
                             <!-- /.card-header -->
-                            <div class="card-body p-0">
-                                <table class="table">
-                                    <thead>
-                                    <tr>
-                                        <th style="width: 10px">#</th>
-                                        <th>Name</th>
-                                        <th>Email</th>
-                                        <th>Phone</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach($users as $user)
-                                            <tr>
-                                                <td>{{ $user->id }}</td>
-                                                <td>{{ $user->name }}</td>
-                                                <td>{{ $user->email }}</td>
-                                                <td>{{ $user->phone }}</td>
-                                                <td>
-                                                    <a href="{{ route('user.edit',$user->id) }}" class="btn btn-primary btn-sm">Edit</a>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- /.card-body -->
+                            <!-- form start -->
+                            <form role="form" action="{{ route('user.update',$user->id) }}" method="post">
+                                @csrf
+                                @method('put')
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="name">Name</label>
+                                        <input type="text" value="{{ $user->name }}" name="name" class="form-control" id="name" placeholder="Enter user name">
+                                        @error('name')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Email address</label>
+                                        <input type="email" value="{{ $user->email }}" name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                                        @error('email')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="phone">Phone</label>
+                                        <input type="text" value="{{ $user->phone }}" name="phone" class="form-control" id="phone" placeholder="Enter phone number">
+                                        @error('phone')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
                         </div>
                         <!-- /.card -->
-                        <!-- /.card -->
+
                     </div>
                     <!--/.col (left) -->
                 </div>
