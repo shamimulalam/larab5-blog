db928d8aa9715a25a9a721769da68f3b0564db6d
Trainer PNT <trainer@piit.local>
2020-02-08 13:55:17 +0600

    User delete done

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -772,6 +772,11 @@
                                                 <td>{{ $user->phone }}</td>
                                                 <td>
                                                     <a href="{{ route('user.edit',$user->id) }}" class="btn btn-primary btn-sm">Edit</a>
+                                                    <form action="{{ route('user.destroy',$user->id) }}" method="post">
+                                                        @csrf
+                                                        @method('delete')
+                                                        <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                                    </form>
                                                 </td>
                                             </tr>
                                         @endforeach
