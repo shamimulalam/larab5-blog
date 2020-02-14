ec3813e77b71be6cae1a19bf1b31ee0e89b09f79
trainer pnt <trainer@pnt.com>
2020-02-14 14:35:58 +0600

    Pagination done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,8 @@ class UserController extends Controller
     public function index()
     {
         $data['title'] = 'List of users';
-        $data['users'] = User::all();
+        $data['users'] = User::paginate(1);
+        $data['serial'] = 1;
         return view('admin.user.index',$data);
     }
 
