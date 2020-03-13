be8b39c8b8a394b72d56b40737e67d40b1579079
trainer pnt <trainer@pnt.com>
2020-03-13 15:13:39 +0600

    done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
+        $data['editors_picks'] = Post::where('is_editors_pick',1)->where('status','Published')->limit(4)->get();
         return view('front.index');
     }
 }
