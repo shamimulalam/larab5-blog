b48a3e36373068d7c535d52eda241a20e92161e2
trainer pnt <trainer@pnt.com>
2020-03-14 13:54:37 +0600

    Home page post rendering done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,19 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        $data['editors_picks'] = Post::with('author','category')->where('is_editors_pick',1)
+        $data['editors_picks'] = Post::with('author','category')
+            ->where('is_editors_pick',1)
             ->where('status','Published')
             ->limit(5)->orderBy('id','desc')->get();
-        $data['trendings'] = Post::with('author','category')->where('is_trending',1)
+        $data['trendings'] = Post::with('author','category')
+            ->where('is_trending',1)
             ->where('status','Published')
             ->limit(4)->orderBy('id','desc')->get();
-//        dd($data);
+        $data['featured_categories'] = Category::with(['posts'=>function($query){
+            $query->orderBy('id', 'desc');
+        }])
+            ->where('is_featured',1)->limit(2)->get();
+        $data['latest_posts'] = Post::with('author','category')
+            ->where('status','Published')
+            ->limit(3)->orderBy('id','desc')->get();
+        $data['popular_posts'] = Post::with('author','category')
+            ->where('status','Published')
+            ->limit(4)->orderBy('total_read','desc')->get();
         return view('front.index',$data);
     }
 }
---
+++ resources/views/front/_withOutImageWithSerial.blade.php
@@ -0,0 +1,10 @@
+<div class="trend-entry d-flex">
+    <div class="number align-self-start">{{ ++$id }}</div>
+    <div class="trend-contents">
+        <h2><a href="blog-single.html">{{ $post->title }}</a></h2>
+        <div class="post-meta">
+            <span class="d-block"><a href="#">{{ $post->author->name }}</a> in <a href="#">{{ $post->category->name }}</a></span>
+            <span class="date-read">{{ date('M d',strtotime($post->created_at)) }} <span class="mx-1">&bullet;</span> {{ $post->total_read }} <span class="icon-star2"></span></span>
+        </div>
+    </div>
+</div>
